c75150c5dc35173e80e90c2c7c16147f395cb5ed
Chonita1 <chonita@starpower.net>
2021-08-03 21:21:51 -0400

    "created a class and instantiation"

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log("hello")
+class Tamagotchi {
+    constructor (name) {
+        this.name = name;
+        this.hunger = 1;
+        this.sleepiness = 1;
+        this.age = 0;
+        this.boredom = 0;
+    }
+}
+const tamagotchi1 = new Tamagotchi('Tammy');
+    console.log(tamagotchi1);
+    console.log(tamagotchi1.name);
+
+// Adding a method to the constructor
+// pet.name = function() {
+// `${this.name} says hello!`;
+// }
